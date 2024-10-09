519ddaa032d099f0840defc3a084b3c5f03d875b
Pawanpreet <pawanpreetkaur529@gmail.com>
2024-10-09 10:27:15 -0600

    update

---
+++ tny_script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Todd Nash
+   Date: 2024-10-08
+
+*/
+
+window.alert("Welcome to Tulsa");
+
+//camelCase
